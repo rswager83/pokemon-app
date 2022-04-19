6b05f1e5584081ce42195cb7daea941180410253
Ryan Swager <101044506+rswager83@users.noreply.github.com>
2022-04-19 08:11:24 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let myName='Bob';
+document.write(myName);
+myName='John Doe';
+document.write(myName);
+
+let size=100;
+let doublesize=size*2;
+let minSize=(doublesize*2)-(size/2);
+document.write(minSize)
