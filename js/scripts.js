ac8a79f43a37ea9c5be3ff14efd12cd41a95cfa2
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-04-20 16:54:47 +0200

    Updated file for lesson 1.2
    Add characters and their different values

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
-alert("Hello World");
+let pokemonList = [];
 
-let favoriteFood= "Pizza";
-document.write(favoriteFood);
+let pokemons =[
+    {name: 'Zacian', height= 9, category: 'Warrior', type: 'Fairy', abilities: 'Intrepid Sword', weaknesses: ['Steel', 'Posion']},
+    {name: 'Grimmsnarl', height= 4, category: 'Bulk Up', type: ['Dark', 'Fairy'], abilities: ['Frisk', 'Prankster'], weaknesses: ['Steel', 'Fairy', 'Posion']},
+    {name: 'Eisue', height= 4, category: 'Penguin', type: 'Ice', abilities: 'Ice Face', weaknesses: ['Fire', 'Steel', 'Fighting', 'Rock']},
+    {name: 'Centiskorch', height= 9, category: 'Radiator', type: ['Fire', 'Bug'], abilities: ['Flash Fire', 'White Smoke'], weaknesses: ['Water', 'Flying', 'Rock']},
+    {name: 'Silicobra', height= 7, category: 'Sand Snake', type: 'Ground', abilities: ['Shed Skin', 'Sand Spit'], weaknesses: ['Water', 'Grass', 'Ice']},
+    {name: 'Drednaw', height= 3, category: 'Bite', type: ['Water', 'Rock'], abilities: ['Shell Armor', 'Strong Jaw'], weaknesses: ['Grass', 'Electric', 'Fighting', 'Ground']}
+];
+
+// Can I also write it in this layout? Thats if I did this correct, lol.
+
+// let pokemons = [{
+//     name: 'Zacian',
+//     height= 9,
+//     category: 'Warrior',
+//     type: 'Fairy',
+//     abilities: 'Intrepid Sword',
+//     weaknesses: ['Steel', 'Posion']
+//     };
+    // {
+    // name: '',
+    // height= '',
+    // type: '',
+    // abilities: '',
+    // weaknesses: ''
+    // };
+
+    // and so on ...
+    
+    
+
+
+]
