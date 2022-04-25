f5d1ac40fecab83addadbba70d1593cc53aeffdc
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-04-25 15:10:46 +0200

    Update scripts.js
    Swapping the pokemonList for loop with  forEach loop. Adding more comments and some retyping of variables.

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,15 @@ let pokemonList = [
     category: 'Warrior',
     type: 'Fairy',
     abilities: 'Intrepid Sword',
-    weaknesses: ['Steel', 'Posion']
+    weaknesses: ['Steel', ' and Posion']
     },
     {
     name: 'Grimmsnarl', 
     height: 4, 
     category: 'Bulk Up', 
-    type: ['Dark', 'Fairy'], 
-    abilities: ['Frisk', 'Prankster'], 
-    weaknesses: ['Steel', 'Fairy', 'Posion']
+    type: ['Dark', ' Fairy'], 
+    abilities: ['Frisk', ' Prankster'], 
+    weaknesses: ['Steel', ' Fairy', ' Posion']
     },
     {
     name: 'Eisue', 
@@ -22,48 +22,64 @@ let pokemonList = [
     category: 'Penguin', 
     type: 'Ice', 
     abilities: 'Ice Face', 
-    weaknesses: ['Fire', 'Steel', 'Fighting', 'Rock']
+    weaknesses: ['Fire', ' Steel', ' Fighting', ' Rock']
     },
     {
     name: 'Centiskorch', 
     height: 9, 
     category: 'Radiator', 
-    type: ['Fire', 'Bug'], 
-    abilities: ['Flash Fire', 'White Smoke'], 
-    weaknesses: ['Water', 'Flying', 'Rock']
+    type: ['Fire', ' Bug'], 
+    abilities: ['Flash Fire', ' White Smoke'], 
+    weaknesses: ['Water', ' Flying', ' Rock']
     },
     {
     name: 'Silicobra', 
     height: 7, 
     category: 'Sand Snake', 
     type: 'Ground', 
-    abilities: ['Shed Skin', 'Sand Spit'], 
-    weaknesses: ['Water', 'Grass', 'Ice']
+    abilities: ['Shed Skin', ' Sand Spit'], 
+    weaknesses: ['Water', ' Grass', ' Ice']
     },
     {
     name: 'Drednaw', 
     height: 3, 
     category: 'Bite',
-    type: ['Water', 'Rock'], 
-    abilities: ['Shell Armor', 'Strong Jaw'], 
-    weaknesses: ['Grass', 'Electric', 'Fighting', 'Ground']
+    type: ['Water', ' Rock'], 
+    abilities: ['Shell Armor', ' Strong Jaw'], 
+    weaknesses: ['Grass', ' Electric', ' Fighting', ' Ground']
     }
 ];
 
-console.log(pokemonList);
+// console.log(pokemonList);
 
 
-// Loop for list of Pokemons on page
+//  for loop for list of Pokemons on page
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 4) { //use if-else seperate items and show special value/result 
-        document.write(" Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " He's tiny!");
-    }else {  
-        document.write(" Name: " + pokemonList[i].name + " (Height : " + pokemonList[i].height + ")")};
+// for (let i = 0; i < pokemonList.length; i++) {
+    // if (pokemonList[i].height < 4) { //use if-else seperate items and show special value/result 
+    //     document.write(" Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " He's tiny!");
+    // }else {  
+    //     document.write(" Name: " + pokemonList[i].name + " (Height : " + pokemonList[i].height + ")")};
     // document.write(`Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
     // document.write(`Category: ${pokemonList[i].category}`);
     // document.write(`Type: ${pokemonList[i].type}`);
     // document.write(`Ability: ${pokemonList[i].abilities}`);
     // document.write(`Weakness: ${pokemonList[i].weaknesses}`);
-    document.write("<br>");
-};
+    // document.write("<br>");
+// };
+
+// forEach Loop for name and height
+pokemonList.forEach(function(pokemon) {
+    // console.log(pokemon.name + ' is ' + pokemon.height + ' feet tall.');
+    // document.write(pokemon.name + ' is a type ' + pokemon.type + " pokemon and is " + pokemon.height + ' feet tall.');
+   
+    if (pokemon.height < 4 ) {
+        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + " type and he's tiny!</p>They are category "
+        + pokemon.category + ' and have ' + pokemon.abilities + ' abilities and have ' + pokemon.weaknesses + ' weaknesses.');
+    }else{
+        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + ' type.</p>They are category '
+        + pokemon.category + ' and have ' + pokemon.abilities + ' abilities and have ' + pokemon.weaknesses + ' weaknesses.');}
+    // document.write("<br>");
+});
+
+
