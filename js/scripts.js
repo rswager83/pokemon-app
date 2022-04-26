bca4cf25423207a3116232d35ac018847b221f3e
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-04-26 08:19:33 +0200

    File update
    Adding IIFE and cleaning up files

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,96 @@
+// Added IIFE to avoid accessing the global state
+
+let pokemonRepository= (function(){
+
 // List of Pokemons and their attributes
+    
 let pokemonList = [
-    {
-    name: 'Zacian',
-    height: 9,
-    category: 'Warrior',
-    type: 'Fairy',
-    abilities: 'Intrepid Sword',
-    weaknesses: ['Steel', ' and Posion']
-    },
-    {
-    name: 'Grimmsnarl', 
-    height: 4, 
-    category: 'Bulk Up', 
-    type: ['Dark', ' Fairy'], 
-    abilities: ['Frisk', ' Prankster'], 
-    weaknesses: ['Steel', ' Fairy', ' Posion']
-    },
-    {
-    name: 'Eisue', 
-    height: 4, 
-    category: 'Penguin', 
-    type: 'Ice', 
-    abilities: 'Ice Face', 
-    weaknesses: ['Fire', ' Steel', ' Fighting', ' Rock']
-    },
-    {
-    name: 'Centiskorch', 
-    height: 9, 
-    category: 'Radiator', 
-    type: ['Fire', ' Bug'], 
-    abilities: ['Flash Fire', ' White Smoke'], 
-    weaknesses: ['Water', ' Flying', ' Rock']
-    },
-    {
-    name: 'Silicobra', 
-    height: 7, 
-    category: 'Sand Snake', 
-    type: 'Ground', 
-    abilities: ['Shed Skin', ' Sand Spit'], 
-    weaknesses: ['Water', ' Grass', ' Ice']
-    },
-    {
-    name: 'Drednaw', 
-    height: 3, 
-    category: 'Bite',
-    type: ['Water', ' Rock'], 
-    abilities: ['Shell Armor', ' Strong Jaw'], 
-    weaknesses: ['Grass', ' Electric', ' Fighting', ' Ground']
+        {
+        name: 'Zacian',
+        height: 9,
+        category: 'warrior',
+        type: 'fairy',
+        abilities: 'intrepid sword',
+        weaknesses: ['Steel', ' and Posion']
+        },
+        {
+        name: 'Grimmsnarl', 
+        height: 4, 
+        category: 'bulk up', 
+        type: ['dark', ' fairy'], 
+        abilities: ['frisk', ' prankster'], 
+        weaknesses: ['steel', ' fairy', ' posion']
+        },
+        {
+        name: 'Eisue', 
+        height: 4, 
+        category: 'penguin', 
+        type: 'ice', 
+        abilities: 'ice face', 
+        weaknesses: ['fire', ' steel', ' fighting', ' rock']
+        },
+        {
+        name: 'Centiskorch', 
+        height: 9, 
+        category: 'radiator', 
+        type: ['fire', ' bug'], 
+        abilities: ['flash fire', ' white smoke'], 
+        weaknesses: ['water', ' flying', ' rock']
+        },
+        {
+        name: 'Silicobra', 
+        height: 7, 
+        category: 'sand snake', 
+        type: 'ground', 
+        abilities: ['shed skin', ' sand spit'], 
+        weaknesses: ['water', ' grass', ' ice']
+        },
+        {
+        name: 'Drednaw', 
+        height: 3, 
+        category: 'bite',
+        type: ['water', ' rock'], 
+        abilities: ['shell armor', ' strong jaw'], 
+        weaknesses: ['grass', ' electric', ' fighting', ' ground']
+        }
+    ];
+
+    //Defining add function to add pokemon to list
+
+    function add(pokemon){
+        pokemonList.push(pokemon);
     }
-];
 
-// console.log(pokemonList);
+    //Defining getAll function to return pokemonList
+    
+    function getAll(){
+        return pokemonList;
+    }
+
+    //returning getAll and add functions
+    
+    return{
+        add:add,
+        getAll:getAll
+    }
 
 
-//  for loop for list of Pokemons on page
+})();
 
-// for (let i = 0; i < pokemonList.length; i++) {
-    // if (pokemonList[i].height < 4) { //use if-else seperate items and show special value/result 
-    //     document.write(" Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " He's tiny!");
-    // }else {  
-    //     document.write(" Name: " + pokemonList[i].name + " (Height : " + pokemonList[i].height + ")")};
-    // document.write(`Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
-    // document.write(`Category: ${pokemonList[i].category}`);
-    // document.write(`Type: ${pokemonList[i].type}`);
-    // document.write(`Ability: ${pokemonList[i].abilities}`);
-    // document.write(`Weakness: ${pokemonList[i].weaknesses}`);
-    // document.write("<br>");
-// };
+pokemonRepository.add({ name: 'Pikachu', height: 3, category: 'mouse', type: 'electric', abilities: 'static', weaknesses: 'ground'});
+console.log(pokemonRepository.getAll());
 
 // forEach Loop for name and height
-pokemonList.forEach(function(pokemon) {
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     // console.log(pokemon.name + ' is ' + pokemon.height + ' feet tall.');
     // document.write(pokemon.name + ' is a type ' + pokemon.type + " pokemon and is " + pokemon.height + ' feet tall.');
    
     if (pokemon.height < 4 ) {
-        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + " type and he's tiny!</p>They are category "
-        + pokemon.category + ' and have ' + pokemon.abilities + ' abilities and have ' + pokemon.weaknesses + ' weaknesses.');
+        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + " type and he's tiny!</p>It is a "
+        + pokemon.category + ' and has ' + pokemon.abilities + ' abilities and has ' + pokemon.weaknesses + ' weaknesses.');
     }else{
-        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + ' type.</p>They are category '
-        + pokemon.category + ' and have ' + pokemon.abilities + ' abilities and have ' + pokemon.weaknesses + ' weaknesses.');}
-    // document.write("<br>");
+        document.write('<p><b> Name:</b> ' + pokemon.name + ' (Height: ' + pokemon.height + ') is a ' + pokemon.type + ' type.</p>It is a '
+        + pokemon.category + ' and has ' + pokemon.abilities + ' abilities and has ' + pokemon.weaknesses + ' weaknesses.');}
+        document.write("<br>");
 });
-
-
